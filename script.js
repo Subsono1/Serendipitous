4ff3a2ba5c0bf862305c56efc8eabec9c0222a1b
Sebastian <seba@Sebastians-MBP.lan>
2020-07-15 10:02:05 -0400

    some functions done, and starting css styling

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-const DOMAIN = 'https://cros-anywhere.tastedive.com/'
-const KEY = '378732-Sebastia-0KJQN855'
-const BASE_URL = `${DOMAIN}api/similar?k=${KEY}`
+const DOMAIN = "https://corsanywhere.herokuapp.com/tastedive.com/";
+const KEY = "378732-Sebastia-0KJQN855";
+const BASE_URL = `${DOMAIN}api/similar?k=${KEY}&`;
 
-async function getMusic() {
+async function getMusic(e) {
+  
  
   try {
-    const response = await axios.get(BASE_URL)
-    console.log(response)
+    const input = document.querySelector("#input");
+    const response = await axios.get(`${BASE_URL}q=&info${input.value}`);
+    console.log(response);
+    //data that shows the the array.
+    renderSearch(response.data.Similar.Results)
   } catch (error) {
-    console.log(`This is an error : ${error}`)
+    console.log(`This is an error : ${error}`);
   }
-}getMusic()
+
+  
+}
+getMusic()
+
+
+
+
+const renderSearch = (allSearch) => {
+
+  allSearch.forEach((search) => {
+    console.log(search)
+    const newSearch = document.createElement('article')
+    newSearch.innerText = search.Name
+    //to append it it is parent node and append child
+    document.querySelector('#results').appendChild(newSearch)
+   
+  })
+  
+};
+
+
+const button = document.querySelector("#search")
+button.addEventListener('click', getMusic )
