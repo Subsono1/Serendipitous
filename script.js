6e710cfb6fbcffd395c1dd878008a5df66ec60a8
Sebastian <seba@Sebastians-MBP.lan>
2020-07-16 23:26:50 -0400

    updated some codes and commented

--- script.js
+++ script.js
@@ -1,72 +1,71 @@
 
-const KEY = `378732-Sebastia-0KJQN855`
-const form = document.querySelector('form')
+//global variables for API request and event listener. 
+const KEY = `378732-Sebastia-0KJQN855`;
+const form = document.querySelector("form");
 
-form.addEventListener('submit', getInput)
+
+//to get the input from the submit button. 
+form.addEventListener("submit", getInput);
 
 function getInput(e) {
-  e.preventDefault()
-  
-  const musicMovieValue = document.querySelector('#music-movie').value
-  
-  console.log(musicMovieValue)
-  
-  getMusicMovie(musicMovieValue)
+  e.preventDefault();
+
+  const musicMovieValue = document.querySelector("#music-movie").value;
+
+  console.log(musicMovieValue);
+
+  getMusicMovie(musicMovieValue);
 }
+// to get API request
 async function getMusicMovie(band, movie) {
   try {
-    
-    const URL = `https://corsanywhere.herokuapp.com/tastedive.com/api/similar?q=${band}%2C${movie}&k=${KEY}&info=1&limit=5`
-    
-    const response = await axios.get(URL)
-    
+    const URL = `https://corsanywhere.herokuapp.com/tastedive.com/api/similar?q=${band}%2C${movie}&k=${KEY}&info=1&limit=10`;
+
+    const response = await axios.get(URL);
+
     console.log(response.data.Similar.Results);
     //data that shows the the array.
-    
-    removeLastSearch()
-    
-    renderSearch(response.data.Similar.Results)
+
+    removeLastSearch();
+
+    renderSearch(response.data.Similar.Results);
   } catch (error) {
-    
     console.log(`This is an error : ${error}`);
   }
 }
 
-
-//////////////////////
+//to get all data from the database
 const renderSearch = (allSearch) => {
   allSearch.forEach((search) => {
-    console.log(search)
-    const container = document.createElement('section')
-    const newSearch = document.createElement('article')
-    newSearch.innerText = search.Name
-    const searchTeaser = document.createElement('p')
-    searchTeaser.innerText = search.wTeaser
-    const video = document.createElement('iframe')
-    video.src = search.yUrl
+    console.log(search);
+    const container = document.createElement("section");
+    const newSearch = document.createElement("article");
+    newSearch.innerText = search.Name;
+    const searchTeaser = document.createElement("p");
+    searchTeaser.innerText = search.wTeaser;
+    const video = document.createElement("iframe");
+    video.src = search.yUrl;
     //to append it to parent node and append child
-    container.appendChild(newSearch)
-    container.appendChild(searchTeaser)
-    container.appendChild(video)
-    document.querySelector('.results').appendChild(container)
-
-  })
+    container.appendChild(newSearch);
+    container.appendChild(searchTeaser);
+    container.appendChild(video);
+    document.querySelector(".results").appendChild(container);
+  });
 };
-
-// function 
-// document.querySelector('section').style =
-
-/////
-
-
-//////////
-
-
+//remove last search from the DOM
 const removeLastSearch = () => {
-  const musicMovie = document.querySelector('.results');
+  const musicMovie = document.querySelector(".results");
   while (musicMovie.lastChild) {
-    musicMovie.removeChild(musicMovie.lastChild)
+    musicMovie.removeChild(musicMovie.lastChild);
   }
-  
+};
+//to scroll to top funtion.
+const randomButton = document.querySelector("#to-top");
 
+function toScroll() {
+  window.scrollTo({
+    top: -400,
+    behavior: "smooth",
+  });
 }
+randomButton.addEventListener("click", toScroll);
